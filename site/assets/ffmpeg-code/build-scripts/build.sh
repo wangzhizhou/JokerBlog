0efb24febb91d74923b1db8ef885abf91fa8727b
wangzhizhou <wangzhizhou@baofeng.com>
2017-10-20 17:16:45 +0800

    refine the project

--- site/assets/ffmpeg-code/build-scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#usr/bin/env bash
-
-g++ $1 -I/usr/local/include -L/usr/local/lib -lavformat -lavfilter -lavutil -lavcodec -lswscale 
--- site/assets/ffmpeg-code/experiment-code/build.sh
+++
@@ -1,1 +0,0 @@
-g++ $1 -I/usr/local/include -L/usr/local/lib -lavcodec -lavformat -lswscale -lavfilter -lavutil -lx264 -lz -lm -lopencv_imgproc -lopencv_highgui -lopencv_core
